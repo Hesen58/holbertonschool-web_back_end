53a15f01437db54d67b97689f78140438d58da8d
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-05-12 22:52:48 +0400

    12th task and final

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient()
+    db = client.logs
+    count = db.nginx.count_documents({})
+    print(f"{count} logs\nMethods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        methodcount = db.nginx.count_documents({"method": method})
+        print(f"\tmethod {method}: {methodcount}")
+    statusGET = db.nginx.count_documents({"method": "GET", "path": "/status"})
+    print(f"{statusGET} status check")
