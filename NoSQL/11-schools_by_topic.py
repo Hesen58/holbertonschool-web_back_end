749b9573209bd9cab4a1f7249ab5ffabd0339f32
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-05-12 22:23:07 +0400

    11th one

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Something useful"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Something more useful"""
+    return mongo_collection.find({"topic": topic})
