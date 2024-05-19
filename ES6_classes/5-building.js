ba76ca34f71f2af009ce2baeb954ab33b5de26c1
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-05-19 16:08:26 +0400

    eslint fix

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,26 +1,25 @@
 export default class Building {
-    constructor(sqft) {
-        if (this.constructor === Building) {
-            throw new Error('Class extending Building must override evacuationWarningMessage')
-        }
-        if (typeof sqft !== 'number') {
-            throw new TypeError('Sqft must be a number')
-        }
-        this._sqft = sqft
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
     }
+    this._sqft = sqft;
+  }
 
-    get sqft() {
-        return this._sqft;
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Sqft must be a number');
     }
+    this._sqft = value;
+  }
 
-    set sqft(value) {
-        if (typeof value !== 'number') {
-            throw new TypeError('Sqft must be a number')
-        }
-        this._sqft = value
+  evacuationWarningMessage() {
+    if (this.constructor === Building) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-
-    evacuationWarningMessage() {
-
-    }
+  }
 }
