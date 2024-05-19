b8e243bf1dc26b6fbf12abfe55ca1fe47002f192
Hesen58 <hesenisgenderov.hi.hi@gmail.com>
2024-05-19 16:16:29 +0400

    5th and 6th

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -3,7 +3,7 @@ export default class Building {
     if (typeof sqft !== 'number') {
       throw new TypeError('Sqft must be a number');
     }
-    this._sqft = sqft;
+    this.sqft = sqft;
   }
 
   get sqft() {
@@ -18,7 +18,7 @@ export default class Building {
   }
 
   evacuationWarningMessage() {
-    if (this.constructor === Building) {
+    if (this.constructor !== Building) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
